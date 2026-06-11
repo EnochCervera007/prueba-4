cf1bbb0fcf43ac7f197ea06cdc6cfe67348ce478
EnochCervera007 <cerveraenoch@gmail.com>
2026-06-11 18:05:11 +0200

    subido

---
+++ prueba.js
@@ -0,0 +1,4 @@
+let numero1 = parseInt(prompt('introdcuce el primer numero'));
+let numero2 = parseInt(prompt('introduce el segundo numero'));
+let suma = numero1 + numero2;
+document.write('El resultado de la suma es: ' + suma);
